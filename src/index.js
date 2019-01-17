c89e5c2e6819e9beb41212391dd01bceb2f6d783
jepras <jeprasher@gmail.com>
2019-01-17 09:59:47 +0100

    index cleaned

--- src/index.js
+++ src/index.js
@@ -1,55 +1,44 @@
+// react
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+
+// redux
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
-import "./index.css";
-import App from "./App";
-
-/* import promise from "redux-promise";
-import logger from "redux-logger"; */
 import thunk from "redux-thunk";
 
+// firebase
 import { reduxFirestore, getFirestore } from "redux-firestore";
 import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+
+// import from tree
+import "./index.css";
+import App from "./App";
 import rootReducer from "./store/reducers";
 import fbConfig from "./config/fbConfig";
 
-/* let middleware = applyMiddleware(
-  thunk.withExtraArgument({ getFirebase, getFirestore }),
-  promise,
-  logger
-); */
+// devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// create store
 const store = createStore(
   rootReducer,
-  compose(
+  composeEnhancers(
+    // enhance with Firebase
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    // enable profile
     reactReduxFirebase(fbConfig, {
       userProfile: "users",
       useFirestoreForProfile: true,
       attachAuthIsReady: true
-    }), // redux binding for firebase
-    reduxFirestore(fbConfig) // redux bindings for firestore
+    }),
+    // redux binding for firebase
+    reduxFirestore(fbConfig) // app data from https://console.firebase.google.com/project/scan2sheet-1543863118125/overview
   )
 );
 
-/* let store = createStore(
-  rootReducer,
-  compose(
-    middleware,
-    reduxFirestore(fbConfig),
-    // retrieve profile info from firebase
-    reactReduxFirebase(fbConfig, {
-      useFirestoreForProfile: true,
-      userProfile: "users",
-      attachAuthIsReady: true
-    })
-    /*     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-     
-  )
-); */
-
+// when auth is ready, router & app with store wrapped.
 store.firebaseAuthIsReady.then(() => {
   ReactDOM.render(
     <Provider store={store}>
